4f16bbd0ca856a8eb9b3beabd72879c05352a8f6
amitpundir992 <pundiramit06@gmail.com>
2024-11-22 16:48:24 +0530

    fifth

--- src/db/db.js
+++ src/db/db.js
@@ -3,7 +3,7 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
   try {
-    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+    const connectionInstance = await mongoose.connect(`mongodb+srv://amitpundir992:bankokhumayenge@cluster0.dsydjpl.mongodb.net/${DB_NAME}`);
     console.log("Mongodb connected || host: ",connectionInstance.connection.host);
   } catch (error) {
     console.log("MONGODB connection Failed", error);
