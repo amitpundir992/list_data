2321dbbdde36484a0149f42f6e29edf5fe4dece8
amitpundir992 <pundiramit06@gmail.com>
2024-11-22 17:35:08 +0530

    addon

--- index.js
+++ index.js
@@ -1,14 +1,59 @@
-import dotenv from "dotenv";
-import { connectDB } from "./src/db/db.js";
 import express from "express";
-import { Data } from "./src/models/user.model.js";
+import mongoose from "mongoose";
 
 const app = express();
+const PORT =8000;
 
 
-dotenv.config({
-  path: "./.env",
-});
+// Connection
+mongoose.connect(`mongodb+srv://amitpundir992:bankokhumayenge@cluster0.dsydjpl.mongodb.net/mongodata`)
+        .then(() => console.log("MOngoDB Connected"))
+        .catch((err) => console.log("Mongo Error", err));
+
+// Schema
+const userSchema = new mongoose.Schema(
+  {
+    status: {
+      type: String,
+    },
+    credentialTitle: {
+      type: String,
+    },
+    approach: {
+      type: String,
+      required: true,
+    },
+    valueProvided: {
+      type: String,
+    },
+    industry: {
+      type: String,
+      required: true,
+    },
+    keyBuyer: {
+      type: String,
+    },
+    technologiesUsed: {
+      type: String,
+    },
+    clientID: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Client", // Assuming a reference to a 'Client' model
+    },
+    projectID: {
+      type: String,
+      required: true,
+    },   
+  }
+);
+
+const Data = mongoose.model("data", userSchema);
+
+// Middleware - Plugin
+app.use(express.urlencoded({ extended : false }))    // helps in taking req.body
+
+
+
 
 app.get("/get-data", async (req, res) => {
   try {
@@ -19,12 +64,5 @@ app.get("/get-data", async (req, res) => {
   }
 });
 
-connectDB()
-  .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
-      console.log(`Server is running at port ${process.env.PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.log("MONGO db connection failed !!! ", err);
-  });
+app.listen(PORT, () => console.log(`Server started at port: ${PORT}`));
+ 
